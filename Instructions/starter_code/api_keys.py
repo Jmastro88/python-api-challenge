78615db709f39496408d2c8c7b36b8bceef8c604
Jonpaul Mastropaolo <Jmastropaolo@csu.fullerton.edu>
2020-02-01 21:15:24 -0800

    Final API HW

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "bddcbd914de8d96a512b7707f4216771"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBRNQHLDYVyHrybRg-vHhm_HUNpPVlusiA"
